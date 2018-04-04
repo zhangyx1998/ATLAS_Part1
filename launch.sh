ee6d4c5e0c9aa27a06d3e4a95817607ad9d4786d
Yuxuan Zhang <37485759+zhangyx1998@users.noreply.github.com>
2018-04-04 11:58:12 -0700

    Version V1.01

--- launch.sh
+++ launch.sh
@@ -3,11 +3,11 @@
 C_DIR="$( cd "$( dirname "$0" )" && pwd )"
 #echo $C_DIR
 source "$C_DIR"/config.sh
-TS_DT=`date "+%Y-%m-%d %H:%M:%S"`
-#echo $TS_DT
-TS_HS=`date -d "$TS_DT" +%s`
-#echo $TS_HS
-TS=$((TS_HS*1000+`date "+%N"`/1000000))
+TS_SC=(`date +%s`)
+TS_MS=(`date "+%N"`/1000000)
+TS_MS=${TS_SC#0}
+TS=$[$TS_SC*1000+TS_MS]
 #echo $TS
-AIO_timestamp="${AIO_timestamp} $TS"
-python $C_DIR'/'$AIO_path $AIO_Debug $AIO_port $AIO_baudrate $AIO_timeout $AIO_timestamp $AIO_host $AIO_user $AIO_password $AIO_Database $AIO_Table $AIO_ErrorTable $AIO_ErrorFile $AIO_InputExpect
+AIO_timestamp="${AIO_timestamp} ${TS}"
+#echo $AIO_timestamp
+python $C_DIR'/'$AIO_path'_'$Version'.py' $AIO_Debug $AIO_port $AIO_baudrate $AIO_timeout $AIO_timestamp $AIO_host $AIO_user $AIO_password $AIO_Database $AIO_Table $AIO_LogTable $AIO_LogFile $AIO_InputExpect $1
